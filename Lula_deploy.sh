0fbb81819cc72f51b636cfbed89d55c82c174210
Lulliter <lmm76@georgetown.edu>
2018-12-13 16:23:36 -0500

    rebuil on Thu Dec 13 16:23:33 EST 2018

---
+++ Lula_deploy.sh
@@ -0,0 +1,29 @@
+	# Go To (git) ./ folder
+	cd ./08_DataMEX_site
+	ls
+	git remote show origin 
+
+# check status
+git status
+
+# Add ALL changes to git Index.
+git add -A
+
+# Create Std commit "message"....
+msg="rebuil on `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+# ... Commit Those changes.
+git commit -m "$msg"
+
+
+# Push source and build repos.
+git push origin master
+
+# # Come Back up to the Project Root
+# cd ..
+# 
+# # Delete the local repository in the shell:
+# rm -rf myrepo/
---
+++ build_site.R
@@ -0,0 +1,18 @@
+# ======= https://bookdown.org/yihui/rmarkdown/rmarkdown-site.html
+
+# ======= Set our working directory. 
+#This helps avoid confusion if our working directory is 
+# not our site but the .Rproject. 
+# setwd("~/GoogleDrive/03_d_R-projects/MiniSiteTemplate")
+
+
+# ======= list which files will be removed
+rmarkdown::clean_site(preview = TRUE)
+# 
+# ======= actually remove the files
+rmarkdown::clean_site()
+
+
+# =======render your sweet site. 
+rmarkdown::render_site()
+
