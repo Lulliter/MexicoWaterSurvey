a90edbf1a191574d2431a480312ff7400bd227b9
Lulliter <lmm76@georgetown.edu>
2018-12-24 18:24:55 -0500

    rebuilt on Mon Dec 24 18:24:54 EST 2018

--- lib/pandoc.R
+++
@@ -1,7 +0,0 @@
-library(pander)
-
-panderOptions('table.split.table', Inf)
-panderOptions('table.split.cells', Inf)
-panderOptions('keep.line.breaks', TRUE)
-panderOptions('table.style', 'multiline')
-panderOptions('table.alignment.default', 'left')
